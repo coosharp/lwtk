af56624489685ceb2f2662d039ba033f112fcde5
coosharp <coosharp@163.com>
2026-01-27 14:58:05 +0800

    feat

--- lwtk/inc/lw_timer.h
+++ lwtk/inc/lw_timer.h
@@ -19,11 +19,25 @@ extern "C" {
 /*********************
  *      INCLUDES
  *********************/
-
+#include <stdint.h>
 /*********************
  *      DEFINES
  *********************/
+typedef struct lw_timer lw_timer_t;
+typedef void (* lw_timer_cb_t)(lw_timer_t * timer, void * user_data);
 
+struct lw_timer
+{
+    lw_timer_t * next;
+    lw_timer_t * prev;
+    uint32_t period;           /**< How often the timer should run */
+    uint32_t last_run;         /**< Last time the timer ran */
+    lw_timer_cb_t timer_cb;    /**< Timer function */
+    void * user_data;          /**< Custom user data */
+    int32_t repeat_count;      /**< 1: One time;  -1 : infinity;  n>0: residual times */
+    uint32_t paused : 1;
+    uint32_t auto_delete : 1;
+};
 /**********************
  *      TYPEDEFS
  **********************/
@@ -32,7 +46,20 @@ extern "C" {
 *  GLOBAL PROTOTYPES
  **********************/
 
+static inline void * lw_timer_get_user_data(lw_timer_t * timer)
+{
+    return timer->user_data;
+}
 
+static inline bool lw_timer_is_paused(lw_timer_t * timer)
+{
+    return timer->paused != 0;
+}
+
+static inline uint32_t lw_timer_get_period(lw_timer_t * timer)
+{
+    return timer->period;
+}
 /**********************
  *      MACROS
  **********************/
--- lwtk/src/lw_timer.c
+++ lwtk/src/lw_timer.c
@@ -33,19 +33,98 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-
+static lw_timer_t * alloc_timer(void);
+static void free_timer(lw_timer_t * timer);
 /**********************
  *  STATIC VARIABLES
  **********************/
-
+static lw_timer_t * gs_mempool = NULL;
+static lw_timer_t * free_list = NULL;     
+static lw_timer_t * used_list = NULL; 
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/ 
+void lw_timer_init(void)
+{
 
+}
+
+lw_timer_t * lw_timer_create()
+{
+
+}
+
+void lw_timer_delete(lw_timer_t * timer)
+{
+
+}
+
+void lw_timer_pause(lw_timer_t * timer)
+{
+
+}
+
+void lw_timer_resume(lw_timer_t * timer)
+{
+
+}
+
+void lw_timer_ready(lw_timer_t * timer)
+{
+
+}
+
+void lw_timer_reset(lw_timer_t * timer)
+{
+
+}
+
+void lw_timer_set_repeat_count(lw_timer_t * timer, int32_t repeat_count)
+{
+
+}
+
+void lw_timer_set_auto_delete(lw_timer_t * timer, bool auto_delete)
+{
+
+}
+
+void lw_timer_set_period(lw_timer_t * timer, uint32_t period)
+{
+
+}
+
+void lw_timer_set_callback(lw_timer_t * timer, lw_timer_cb_t timer_cb, void * user_data)
+{
+
+}
+
+void lw_timer_set_user_data(lw_timer_t * timer,  void * user_data)
+{
+
+}
+
+void lw_timer_increase_tick(uint32_t tick_period)
+{
+
+} 
+
+void lw_timer_handler(void)
+{
+
+}
 /**********************
  *   STATIC FUNCTIONS
  **********************/
+static lw_timer_t * alloc_timer(void)
+{
 
+}
+
+static void free_timer(lw_timer_t * timer)
+{
+
+}
 
 
 
